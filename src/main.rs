3755619c38b38545ddcd266a6d78f47b611d1d82
Matt Keeter <matt.j.keeter@gmail.com>
2020-09-03 12:56:07 -0400

    Added TOFU

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,57 @@
 use std::io::{stdout, Read, Write};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::net::TcpStream;
 
-struct GeminiCertificateVerifier { }
+struct GeminiCertificateVerifier {
+    db: RwLock<sled::Tree>
+}
 
 impl rustls::ServerCertVerifier for GeminiCertificateVerifier {
     fn verify_server_cert(&self,
-                          roots: &rustls::RootCertStore,
+                          _roots: &rustls::RootCertStore,
                           presented_certs: &[rustls::Certificate],
                           dns_name: webpki::DNSNameRef<'_>,
-                          ocsp_response: &[u8])
+                          _ocsp_response: &[u8])
         -> Result<rustls::ServerCertVerified, rustls::TLSError>
     {
-        Ok(rustls::ServerCertVerified::assertion())
+        use rustls::{TLSError, ServerCertVerified};
+
+        if presented_certs.len() < 1 {
+            return Err(TLSError::NoCertificatesPresented)
+        }
+
+        let dns_name = dns_name.to_owned();
+        let d : &str = AsRef::<str>::as_ref(&dns_name);
+        let r = self.db.read().unwrap().get(&d)
+            .map_err(|e| TLSError::General(e.to_string()))?;
+
+        if let Some(c) = r {
+            if c == presented_certs[0].as_ref() {
+                Ok(ServerCertVerified::assertion())
+            } else {
+                Err(TLSError::WebPKIError(webpki::Error::CertNotValidForName))
+            }
+        } else {
+            println!("Writing new cert to db");
+            self.db.write().unwrap()
+                .insert(d, presented_certs[0].as_ref())
+                .map_err(|e| TLSError::General(e.to_string()))?;
+            Ok(ServerCertVerified::assertion())
+        }
     }
 }
 
 fn main() {
+    let dirs = directories::ProjectDirs::from("com", "mkeeter", "titan")
+        .expect("Could not get project dirs");
+    let db = sled::open(dirs.data_dir())
+        .expect("Could not open db");
+    let certs = db.open_tree("certs")
+        .expect("Could not open certs tree");
+
     let mut config = rustls::ClientConfig::new();
-    config.dangerous().set_certificate_verifier(Arc::new(GeminiCertificateVerifier { }));
+    let verifier = GeminiCertificateVerifier { db: RwLock::new(certs) };
+    config.dangerous().set_certificate_verifier(Arc::new(verifier));
 
     let dns_name = webpki::DNSNameRef::try_from_ascii_str("gemini.circumlunar.space").unwrap();
     let mut sess = rustls::ClientSession::new(&Arc::new(config), dns_name);
@@ -29,6 +62,6 @@ fn main() {
     tls.write("gemini://gemini.circumlunar.space/docs/\r\n".as_bytes()).unwrap();
 
     let mut plaintext = Vec::new();
-    print!("{:?}", tls.read_to_end(&mut plaintext));
+    println!("{:?}", tls.read_to_end(&mut plaintext));
     stdout().write_all(&plaintext).unwrap();
 }
