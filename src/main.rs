fd54e551f8e14128d7d5165b588ed2383da51b3b
Matt Keeter <matt.j.keeter@gmail.com>
2020-09-05 15:56:35 -0400

    Add mechanism for input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{BufRead, Read, Write};
 use std::sync::{Arc};
 use std::net::TcpStream;
 use anyhow::{anyhow, Result};
@@ -11,14 +11,17 @@ use crate::parser::{parse_response_header, parse_text_gemini};
 use crate::protocol::{Document, ResponseHeader, ResponseStatus, Line};
 use crate::tofu::GeminiCertificateVerifier;
 
-fn fetch(target: &str, config: Arc<rustls::ClientConfig>)
+fn fetch<F>(target: &str, config: Arc<rustls::ClientConfig>, reader: F)
     -> Result<(ResponseHeader, Option<Document>)>
+    where F: FnMut(&str, bool) -> Result<String>
 {
-    fetch_(target, config, 0)
+    fetch_(target, config, reader, 0)
 }
 
-fn fetch_(target: &str, config: Arc<rustls::ClientConfig>, depth: u8)
+fn fetch_<F>(target: &str, config: Arc<rustls::ClientConfig>,
+             mut reader: F, depth: u8)
     -> Result<(ResponseHeader, Option<Document>)>
+    where F: FnMut(&str, bool) -> Result<String>
 {
     println!("Fetching {}", target);
     if depth >= 5 {
@@ -58,8 +61,24 @@ fn fetch_(target: &str, config: Arc<rustls::ClientConfig>, depth: u8)
     use ResponseStatus::*;
     match header.status {
         RedirectTemporary | RedirectPermanent =>
-            fetch_(&header.meta, config, depth + 1),
+            fetch_(&header.meta, config, reader, depth + 1),
 
+        Input | SensitiveInput => {
+            let input = reader(&header.meta, header.status == SensitiveInput)?;
+            let has_query = url.query().is_some();
+
+            // Recast the URL variable to be mutable in this block
+            let mut url = url;
+            {   // Modify the URL to include the query string
+                let mut segs = url.path_segments_mut()
+                        .map_err(|_| anyhow!("Could not get path segments"))?;
+                if has_query {
+                    segs.pop();
+                }
+                segs.extend(&["?", &input]);
+            }
+            fetch_(url.as_str(), config, reader, depth + 1)
+        },
         // Only read the response body if we got a Success response status
         Success =>
             if header.meta.starts_with("text/gemini") {
@@ -80,6 +99,14 @@ fn fetch_(target: &str, config: Arc<rustls::ClientConfig>, depth: u8)
     }
 }
 
+fn read_bytes(prompt: &str, _sensitive: bool) -> Result<String> {
+    print!("{}", prompt);
+    std::io::stdout().lock().flush()?;
+    let mut buf = String::new();
+    std::io::stdin().lock().read_line(&mut buf)?;
+    Ok(buf)
+}
+
 fn main() -> Result<()> {
     let dirs = directories::ProjectDirs::from("com", "mkeeter", "titan")
         .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other,
@@ -91,6 +118,6 @@ fn main() -> Result<()> {
     config.dangerous().set_certificate_verifier(Arc::new(verifier));
     let config = Arc::new(config);
 
-    fetch("gemini://gemini.circumlunar.space", config)?;
+    fetch("gemini://gemini.circumlunar.space", config, read_bytes)?;
     Ok(())
 }
