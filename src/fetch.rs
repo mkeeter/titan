796533153d0a62209fd5cb32b381459f9cf3e3d4
Matt Keeter <matt.j.keeter@gmail.com>
2020-09-05 21:57:23 -0400

    Trying to make fetch happen

---
+++ src/fetch.rs
@@ -0,0 +1,101 @@
+use std::io::{Read, Write};
+use std::sync::{Arc};
+use std::net::TcpStream;
+
+use anyhow::{anyhow, Result};
+
+use crate::document::Document;
+use crate::parser::{parse_response_header, parse_text_gemini};
+use crate::protocol::{Line, ResponseHeader, ResponseStatus};
+
+pub trait Fetch {
+    fn config(&self) -> Arc<rustls::ClientConfig>;
+
+    fn input(&mut self, prompt: &str, is_sensitive: bool) -> Result<String>;
+    fn display(&mut self, doc: &Document) -> Result<()>;
+    fn header(&mut self, header: &ResponseHeader) -> Result<()>;
+}
+
+pub fn fetch<F: Fetch>(target: &str, cb: &mut F) -> Result<()> {
+    fetch_(target, cb, 0)
+}
+
+fn fetch_<F: Fetch>(target: &str, cb: &mut F, depth: u8) -> Result<()> {
+    println!("Fetching {}", target);
+    if depth >= 5 {
+        return Err(anyhow!("Too much recursion"));
+    }
+
+    let url = url::Url::parse(target)?;
+    if url.scheme() != "gemini" {
+        return Err(anyhow!("Invalid URL scheme: {}", url.scheme()));
+    }
+
+    let hostname = url.host_str()
+        .ok_or_else(|| anyhow!("Error: no hostname in {}", target))?;
+    let dns_name = webpki::DNSNameRef::try_from_ascii_str(hostname)?;
+    let mut sess = rustls::ClientSession::new(&cb.config(), dns_name);
+
+    let port = url.port().unwrap_or(1965);
+    let mut sock = TcpStream::connect(format!("{}:{}", hostname, port))?;
+    let mut tls = rustls::Stream::new(&mut sess, &mut sock);
+
+    tls.write_all(format!("{}\r\n", target).as_bytes())?;
+
+    let mut plaintext = Vec::new();
+    let rc = tls.read_to_end(&mut plaintext);
+
+    // The server should cleanly close the connection at the end of the
+    // message, which returns an error from read_to_end but is actually okay.
+    if let Err(err) = rc {
+        if err.kind() != std::io::ErrorKind::ConnectionAborted {
+            return Err(err.into());
+        }
+    }
+
+    let (body, header) = parse_response_header(&plaintext).map_err(
+        |e| anyhow!("Header parsing failed: {}", e))?;
+
+    use ResponseStatus::*;
+    match header.status {
+        RedirectTemporary | RedirectPermanent => {
+            return fetch_(&header.meta, cb, depth + 1);
+        },
+
+        Input | SensitiveInput => {
+            let input = cb.input(&header.meta, header.status == SensitiveInput)?;
+            let has_query = url.query().is_some();
+
+            // Recast the URL variable to be mutable in this block
+            let mut url = url;
+            {   // Modify the URL to include the query string
+                let mut segs = url.path_segments_mut()
+                    .map_err(|_| anyhow!("Could not get path segments"))?;
+                if has_query {
+                    segs.pop();
+                }
+                segs.extend(&["?", &input]);
+            }
+            return fetch_(url.as_str(), cb, depth + 1);
+        },
+        // Only read the response body if we got a Success response status
+        Success =>
+            if header.meta.starts_with("text/gemini") {
+                let body = std::str::from_utf8(body)?;
+                let (_, doc) = parse_text_gemini(body).map_err(
+                    |e| anyhow!("text/gemini parsing failed: {}", e))?;
+                cb.display(&doc)?;
+            } else if header.meta.starts_with("text/") {
+                // Read other text/ MIME types as a single preformatted line
+                let body = std::str::from_utf8(body)?;
+                let text = Line::Pre { alt: None, text: body.split('\n').collect() };
+                cb.display(&Document(vec![text]))?;
+            } else {
+                return Err(anyhow!("Unknown meta: {}", header.meta));
+            },
+
+        // Otherwise, invoke the header cb
+        _ => { cb.header(&header)?; }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,42 @@
-use std::io::{BufRead, Read, Write};
+use std::io::{BufRead, Write};
 use std::sync::{Arc};
-use std::net::TcpStream;
-use anyhow::{anyhow, Result};
+
+use anyhow::{Result};
 
 mod document;
 mod protocol;
 mod parser;
 mod tofu;
+mod fetch;
 
 use crate::document::Document;
-use crate::parser::{parse_response_header, parse_text_gemini};
-use crate::protocol::{ResponseHeader, ResponseStatus, Line};
+use crate::fetch::{Fetch, fetch};
+use crate::protocol::{ResponseHeader};
 use crate::tofu::GeminiCertificateVerifier;
-
-fn fetch<R, D, H>(target: &str, config: Arc<rustls::ClientConfig>,
-                  reader_cb: R, doc_cb: D, header_cb: H)
-        -> Result<()>
-    where R: FnMut(&str, bool) -> Result<String>,
-          D: FnMut(&Document) -> Result<()>,
-          H: FnMut(&ResponseHeader) -> Result<()>
-{
-    fetch_(target, config, reader_cb, doc_cb, header_cb, 0)
-}
-
-fn fetch_<R, D, H>(target: &str, config: Arc<rustls::ClientConfig>,
-                   mut reader_cb: R, mut doc_cb: D, mut header_cb: H,
-                   depth: u8)
-        -> Result<()>
-    where R: FnMut(&str, bool) -> Result<String>,
-          D: FnMut(&Document) -> Result<()>,
-          H: FnMut(&ResponseHeader) -> Result<()>
-{
-    println!("Fetching {}", target);
-    if depth >= 5 {
-        return Err(anyhow!("Too much recursion"));
-    }
-
-    let url = url::Url::parse(target)?;
-    if url.scheme() != "gemini" {
-        return Err(anyhow!("Invalid URL scheme: {}", url.scheme()));
-    }
-
-    let hostname = url.host_str()
-        .ok_or_else(|| anyhow!("Error: no hostname in {}", target))?;
-    let dns_name = webpki::DNSNameRef::try_from_ascii_str(hostname)?;
-    let mut sess = rustls::ClientSession::new(&config, dns_name);
-
-    let port = url.port().unwrap_or(1965);
-    let mut sock = TcpStream::connect(format!("{}:{}", hostname, port))?;
-    let mut tls = rustls::Stream::new(&mut sess, &mut sock);
-
-    tls.write_all(format!("{}\r\n", target).as_bytes())?;
-
-    let mut plaintext = Vec::new();
-    let rc = tls.read_to_end(&mut plaintext);
-
-    // The server should cleanly close the connection at the end of the
-    // message, which returns an error from read_to_end but is actually okay.
-    if let Err(err) = rc {
-        if err.kind() != std::io::ErrorKind::ConnectionAborted {
-            return Err(err.into());
-        }
-    }
-
-    let (body, header) = parse_response_header(&plaintext).map_err(
-        |e| anyhow!("Header parsing failed: {}", e))?;
-
-    use ResponseStatus::*;
-    match header.status {
-        RedirectTemporary | RedirectPermanent => {
-            return fetch_(&header.meta, config, reader_cb, doc_cb, header_cb, depth + 1);
-        },
-
-        Input | SensitiveInput => {
-            let input = reader_cb(&header.meta, header.status == SensitiveInput)?;
-            let has_query = url.query().is_some();
-
-            // Recast the URL variable to be mutable in this block
-            let mut url = url;
-            {   // Modify the URL to include the query string
-                let mut segs = url.path_segments_mut()
-                    .map_err(|_| anyhow!("Could not get path segments"))?;
-                if has_query {
-                    segs.pop();
-                }
-                segs.extend(&["?", &input]);
-            }
-            return fetch_(url.as_str(), config, reader_cb, doc_cb, header_cb, depth + 1);
-        },
-        // Only read the response body if we got a Success response status
-        Success =>
-            if header.meta.starts_with("text/gemini") {
-                let body = std::str::from_utf8(body)?;
-                let (_, text) = parse_text_gemini(body).map_err(
-                    |e| anyhow!("text/gemini parsing failed: {}", e))?;
-                doc_cb(&text)?;
-            } else if header.meta.starts_with("text/") {
-                // Read other text/ MIME types as a single preformatted line
-                let body = std::str::from_utf8(body)?;
-                let text = Line::Pre { alt: None, text: body.split('\n').collect() };
-                doc_cb(&Document(vec![text]))?;
-            } else {
-                return Err(anyhow!("Unknown meta: {}", header.meta));
-            },
-
-        // Otherwise, invoke the header cb
-        _ => { header_cb(&header)?; }
-    }
-    Ok(())
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
-fn read_bytes(prompt: &str, _sensitive: bool) -> Result<String> {
-    print!("{}", prompt);
-    std::io::stdout().lock().flush()?;
-    let mut buf = String::new();
-    std::io::stdin().lock().read_line(&mut buf)?;
-    Ok(buf)
-}
+struct Simple(Arc<rustls::ClientConfig>);
 
-fn print_doc(doc: &Document) -> Result<()> {
-    doc.word_wrap(40).pretty_print();
-    Ok(())
-}
+impl Fetch for Simple {
+    fn config(&self) -> Arc<rustls::ClientConfig> { self.0.clone() }
 
-fn print_header(header: &ResponseHeader) -> Result<()> {
-    println!("{:?}", header);
-    Ok(())
+    fn input(&mut self, prompt: &str, _is_sensitive: bool) -> Result<String> {
+        print!("{}", prompt);
+        std::io::stdout().lock().flush()?;
+        let mut buf = String::new();
+        std::io::stdin().lock().read_line(&mut buf)?;
+        Ok(buf)
+    }
+
+    fn display(&mut self, doc: &Document) -> Result<()> {
+        doc.word_wrap(40).pretty_print();
+        Ok(())
+    }
+    fn header(&mut self, header: &ResponseHeader) -> Result<()> {
+        println!("Response header: {:?}", header);
+        Ok(())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -143,13 +52,6 @@ fn main() -> Result<()> {
     config.dangerous().set_certificate_verifier(Arc::new(verifier));
     let config = Arc::new(config);
 
-    fetch("gemini://gemini.circumlunar.space", config, read_bytes, print_doc, print_header)?;
-        /*
-    println!("{:?}", doc);
-    doc.word_wrap(60)
-        .pretty_print();
-
-    println!("{:?}", Document(vec![Line::Text("".to_string())]).word_wrap(40));
-        */
+    fetch("gemini://gemini.circumlunar.space", &mut Simple(config))?;
     Ok(())
 }
