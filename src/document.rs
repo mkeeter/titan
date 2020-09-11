a4c59f9c0dd3dcef5fc701b294d8b57e114bf10f
Matt Keeter <matt.j.keeter@gmail.com>
2020-09-11 09:00:52 -0400

    Make parsing not allocate intermediate vecs

--- src/document.rs
+++ src/document.rs
@@ -11,11 +11,12 @@ pub type WrappedLine<'a> = Line_<'a, (&'a str, bool)>;
 pub struct WrappedDocument<'a>(pub Vec<WrappedLine<'a>>);
 
 impl Document<'_> {
-    fn wrap<'a, F>(s: &'a str, width: usize, mut f: F) -> Vec<WrappedLine<'a>>
-        where F: FnMut((&'a str, bool)) -> WrappedLine<'a>
+    fn wrap<'a, F>(s: &'a str, width: usize, mut f: F)
+        -> Box<dyn Iterator<Item=WrappedLine<'a>> + 'a>
+        where F: 'a + FnMut((&'a str, bool)) -> WrappedLine<'a>
     {
         let default = f(("", true));
-        let mut t: Vec<WrappedLine<'a>> = textwrap::Wrapper::new(width)
+        let mut t = textwrap::Wrapper::new(width)
             .wrap(s)
             .into_iter()
             .map(|b: Cow<'a, str>|
@@ -26,25 +27,27 @@ impl Document<'_> {
                 })
             .zip(std::iter::once(true).chain(std::iter::repeat(false)))
             .map(f)
-            .collect();
+            .peekable();
 
-        if t.is_empty() {
-            t.push(default);
+        if t.peek().is_some() {
+            Box::new(t)
+        } else {
+            Box::new(std::iter::once(default))
         }
-        t
     }
 
-    fn line_wrap<'a>(line: &'a Line, width: usize) -> Vec<WrappedLine<'a>> {
+    fn line_wrap<'a>(line: &'a Line, width: usize)
+        -> Box<dyn Iterator<Item=WrappedLine<'a>> + 'a>
+    {
         use Line_::*;
         match line {
             Text(t) => Self::wrap(t, width, Text),
-            BareLink(url) => vec![BareLink(url)],
-            NamedLink { name, url } => Self::wrap(name, width - 3, |s|
+            BareLink(url) => Box::new(std::iter::once(BareLink(url))),
+            NamedLink { name, url } => Self::wrap(name, width - 3, move |s|
                 NamedLink { url, name: s }),
-            Pre { text, alt } => text.split('\n')
+            Pre { text, alt } => Box::new(text.split('\n')
                 .zip(std::iter::once(true).chain(std::iter::repeat(false)))
-                .map(|(s, i)| Pre { text: (s, i), alt: *alt })
-                .collect(),
+                .map(move |(s, i)| Pre { text: (s, i), alt: *alt })),
             H1(t) => Self::wrap(t, width - 2, H1), // "# "
             H2(t) => Self::wrap(t, width - 3, H2), // "## "
             H3(t) => Self::wrap(t, width - 4, H3), // "### "
